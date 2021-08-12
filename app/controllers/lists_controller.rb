0f3b9a9e5362258116d0f23ba5f1bf5cbe6b99da
thais de rodez <thaisderodez@gmail.com>
2021-08-12 18:27:15 +0200

    lists & bookmark routes & controller complete, style, API seed

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -6,6 +6,7 @@ class ListsController < ApplicationController
 
   def show
     @list = List.find(params[:id])
+    @bookmarks = @list.bookmarks
   end
 
   def new
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  resources :lists, only: [:index, :show, :new, :create]
+  resources :lists, only: [:index, :show, :new, :create] do
+    resources :bookmarks, only: [:new, :create]
+  end
+  resources :bookmarks, only: [:destroy]
 end
